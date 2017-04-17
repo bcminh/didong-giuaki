fad439e4fb399415edd29ef517786669d33ac1a1
Minh Danny <minhdanny@Minhs-Mac.local>
2017-04-18 00:57:50 +0700

    add structure

---
+++ rest_management/rest_management/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rest_management
+//
+//  Created by Minh Danny on 4/17/17.
+//  Copyright © 2017 Bach Chan Minh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
